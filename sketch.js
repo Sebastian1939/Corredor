47cc79964379186f4b06702f86beb500d8aed457
Sebastian1939 <142358017+Sebastian1939@users.noreply.github.com>
2023-10-16 16:33:36 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var path,mainPlayer1;
+
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+
+var distance;
+var gameover,restart;
+function preload(){
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+ 
+}
